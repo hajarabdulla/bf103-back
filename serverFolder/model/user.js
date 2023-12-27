36a8908a7fec07d0f8a40f094909e746a2d5fd3f
= <=>
2023-12-27 10:04:58 +0400

    auth

--- serverFolder/model/user.js
+++ serverFolder/model/user.js
@@ -1,14 +1,26 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+
 const { Schema } = mongoose;
 
 const userSchema = new Schema(
   {
     name: { type: String, require: true },
-    surname: { type: String, require: true },
+    email: {
+      type: String,
+      require: [true, "Please provide an email"],
+    //   unique: true,
+    },
     age: { type: Number },
+    password: { type: String, require: true },
   },
   { timestamps: true }
 );
+
+userSchema.pre("save", async function (next) {
+  this.password = await bcrypt.hash(this.password, 12);
+  next();
+});
 
 const User = mongoose.model("User", userSchema);
 
--- serverFolder/routes/userRouter.js
+++ serverFolder/routes/userRouter.js
@@ -1,10 +1,14 @@
 const express = require("express");
+const userController = require("../controller/userController");
 const authController = require("../controller/authController");
 
 const router = express.Router();
 
-router.get("/users", authController.GetUsers);
-router.get("/users/:id", authController.GetUserById);
-router.post("/users", authController.AddUser);
+router.get("/users", userController.getUsers);
+router.get("/users/:id", userController.getUserById);
+router.delete("/users/:id", userController.deleteUser);
+router.put("/users/:id", userController.updateUser);
+router.post("/auth", authController.signup);
+router.post("/auth/login", authController.signin);
 
 module.exports = router;
